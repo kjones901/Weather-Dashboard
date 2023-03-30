601290627d28ed3f79c47a9276022977b06c9b1e
Kyle Jones <kyle@Kyles-MacBook-Pro.local>
2023-03-30 13:27:55 -0600

    worked on local storage. Have it saving the data to an array in local storage, but currently it's only pulling the weather. It isn't extracting the 5 day forecast from the data yet.

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,13 @@
-var LatLonUrl =
-  "https://api.openweathermap.org/data/2.5/forecast?lat=40.1150133&lon=-111.6547774&appid=fe875364d73fc24b0907b9330ffdd475&units=imperial";
-
 var cityForm = document.getElementById("city-form");
 var cityInput;
 
 cityForm.addEventListener("submit", function (event) {
   event.preventDefault();
   cityInput = document.getElementById("city-input").value;
-  getCityName();
+  getLatLon();
 });
 
-function getCityName() {
+function getLatLon() {
   fetch(
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
       cityInput +
@@ -22,23 +19,35 @@ function getCityName() {
     .then(function (data) {
       var lat = data[0].lat;
       var lon = data[0].lon;
-      getCityLatLon(lat, lon);
+      storeCityData(lat, lon);
     });
 }
 
-function getCityLatLon(lat, lon) {
-  var LatLonUrl =
+function storeCityData(lat, lon) {
+  fetch(
     "https://api.openweathermap.org/data/2.5/forecast?lat=" +
-    lat +
-    "&lon=" +
-    lon +
-    "&appid=fe875364d73fc24b0907b9330ffdd475&units=imperial";
-
-  fetch(LatLonUrl)
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=fe875364d73fc24b0907b9330ffdd475&units=imperial"
+  )
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
+      var cityData = JSON.parse(localStorage.getItem("cityData")) || [];
+
+      var newCityData = {
+        name: data.city.name,
+        temp: data.list[0].main.temp,
+        humidity: data.list[0].main.humidity,
+        weather: data.list[0].weather[0].main,
+        windSpeed: data.list[0].wind.speed,
+      };
+
+      cityData.push(newCityData);
+      localStorage.setItem("cityData", JSON.stringify(cityData));
       console.log(data);
+      console.log(cityData);
     });
 }
