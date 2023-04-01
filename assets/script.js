64530d43fbcec2866cfc60f0f00d2877ffe931e5
Kyle Jones <kyle@Kyles-MacBook-Pro.local>
2023-04-01 17:13:16 -0600

    finished the javascript coding to display the data. Updated the html. Creat ed a clear history function. Added README.md. Dictated in comments how the website function.

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,17 @@
 var cityForm = document.getElementById("city-form");
 var cityInput;
+var weatherData = [];
+var cityTitle = document.getElementById("city-name");
 
+//this takes the input of the user and saves it as a variable to be used by future functions.
 cityForm.addEventListener("submit", function (event) {
   event.preventDefault();
   cityInput = document.getElementById("city-input").value;
   getLatLon();
 });
 
+//this takes the user inputed city name and fetchs the latitude and longitude.
+//it creates a variable called city that logs the name,lat,&lon that is used as parameters in future functions.
 function getLatLon() {
   fetch(
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
@@ -25,9 +30,12 @@ function getLatLon() {
       getWeather(city.lat, city.lon);
       saveCityData(city);
       createHistoryButton(city);
+      cityTitle.innerHTML = data[0].name;
     });
 }
 
+//this takes the lat and lon from getLatLon and fetches the 5 day weather forecast from openweathermap.org
+//it saves the data in an array and calls displayWeather.
 function getWeather(lat, lon) {
   fetch(
     "https://api.openweathermap.org/data/2.5/forecast?lat=" +
@@ -40,21 +48,19 @@ function getWeather(lat, lon) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-      for (var i = 0; i < 40; i += 8) {
-        console.log(i, data.list[i]);
-      }
+      weatherData = data.list;
+      displayWeather();
     });
 }
 
+//this stores the city variable created by getlatlon in local storage.
 function saveCityData(city) {
   var cityData = JSON.parse(localStorage.getItem("cityData")) || [];
-
   cityData.push(city);
-
   localStorage.setItem("cityData", JSON.stringify(cityData));
 }
 
+//this creates clickable buttons in the DOM that can be used to recall data from local storage.
 function createHistoryButton(city) {
   var historyButton = document.createElement("li");
   historyButton.classList.add("history-button");
@@ -62,15 +68,15 @@ function createHistoryButton(city) {
   historyButton.id = "button-" + city.name;
   historyButton.style.cursor = "pointer";
   historyButton.style.color = "darkgreen";
+  historyButton.style.paddingLeft = "10px";
   historyButton.addEventListener("click", function (event) {
     event.preventDefault();
-    var buttonId = historyButton.id;
-    getHistory();
-    console.log(buttonId);
+    displayFromLocal(city.name);
   });
   document.getElementById("city-history").appendChild(historyButton);
 }
 
+//this loads local storage and sends the data to the creatHistoryButton function.
 function loadFromLocalStorage() {
   var cityData = JSON.parse(localStorage.getItem("cityData")) || [];
   for (let i = 0; i < cityData.length; i++) {
@@ -78,8 +84,46 @@ function loadFromLocalStorage() {
   }
 }
 
-function getHistory() {
-  console.log("clicky");
+//this grabs the elements in HTML and edits their contents to display the data pulled by getWeather from openweather's api.
+//using a single for loop it goes through the the html elements as well as the data in the weatherData array.
+//it rotates through the weatherData in increments of 8 because each object is broken into 3hr time blocks, so each 8th block will be 24hrs later.
+function displayWeather() {
+  for (var i = 0; i <= 4; i++) {
+    var dispTemp = document.getElementById("today+" + [i] + "-temp");
+    dispTemp.innerHTML = weatherData[i * 8].main.temp;
+    var dispCond = document.getElementById("today+" + [i] + "-conditions");
+    dispCond.innerHTML = weatherData[i * 8].weather[0].main;
+    var dispWind = document.getElementById("today+" + [i] + "-wind");
+    dispWind.innerHTML = weatherData[i * 8].wind.speed;
+    var dispHumidity = document.getElementById("today+" + [i] + "-humidity");
+    dispHumidity.innerHTML = weatherData[i * 8].main.humidity;
+  }
 }
+
+//this pulls the information from local storage to update the title with the city name and run getWeather with the saved lat & lon.
+function displayFromLocal(city) {
+  var cityData = JSON.parse(localStorage.getItem("cityData"));
+  for (var i = 0; i < cityData.length; i++) {
+    if (cityData[i].name === city) {
+      var lat = cityData[i].lat;
+      var lon = cityData[i].lon;
+      getWeather(lat, lon);
+      cityTitle.innerHTML = city;
+    }
+  }
+}
+
+//this clears local storage and deletes the buttons created in the history.
+function clearLocalStorage(event) {
+  event.preventDefault();
+  localStorage.clear();
+  var historyButtons = document.querySelectorAll(".history-button");
+  for (let i = 0; i < historyButtons.length; i++) {
+    historyButtons[i].remove();
+  }
+}
+
+//this calls clearLocalStorage.
+document.getElementById("clear-button").addEventListener("click", clearLocalStorage);
 
 loadFromLocalStorage();
